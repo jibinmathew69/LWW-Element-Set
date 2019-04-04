d5da92c239e718e1c1164de06854c2524f27d069
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-04-04 15:46:15 +0530

    Add test for add() in Lww

--- tests/test_lww.py
+++ tests/test_lww.py
@@ -3,3 +3,16 @@ This file contains tests for Lww class from LWW.lww module
 '''
 
 from LWW.lww import Lww
+
+def test_add():
+    '''
+    This functions test the successful addition of the element
+    into the LWW
+    :return: None
+    '''
+
+    lww = Lww()
+
+    lww.add(1)
+
+    assert 1 in lww.add_set
