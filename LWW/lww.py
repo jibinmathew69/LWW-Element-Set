410866dc66c1aa5fad0fdfbf33ec0a36f5f5463a
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-04-04 15:26:09 +0530

    Document add method of Lww

--- LWW/lww.py
+++ LWW/lww.py
@@ -10,3 +10,12 @@ class Lww:
     def __init__(self):
         self.add_set = {}
         self.remove_set = {}
+
+    def add(self, element):
+        '''
+        This method adds the element into the add_set dictionary
+        of the Lww, where key is the element and value is the current unix timestamp
+        :param element: Element to be add into LWW
+        :return: None
+        '''
+        pass
