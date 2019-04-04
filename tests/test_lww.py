5c235407179878a8f55caedc54bc7e0ccbd9b2de
jibinmathew691993 <jibinmathew.69@gmail.com>
2019-04-04 16:08:48 +0530

    Add test_remove for LWW remove

--- tests/test_lww.py
+++ tests/test_lww.py
@@ -16,3 +16,19 @@ def test_add():
     lww.add(1)
 
     assert lww.lookup(1)
+
+
+def test_remove():
+    '''
+    This function test the successful removal of the element
+    from the LWW
+    :return: None
+    '''
+
+    lww = Lww()
+
+    lww.remove(1)
+    assert not lww.lookup(1)
+
+    lww.add(1)
+    assert lww.lookup(1)
